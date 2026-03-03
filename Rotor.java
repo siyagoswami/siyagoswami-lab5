0f2c902dc31948b82f65db91f4797429c4319fd1
siyagoswami <siya.goswami7@gmail.com>
2026-03-03 14:05:30 -0500

    implemented the rotate() method in the Rotor class

--- Rotor.java
+++ Rotor.java
@@ -8,13 +8,22 @@ public class Rotor {
         this.rotorValues = new String(v);
         this.startChar = c;
         
-        while(!this.rotate());
+        // rotate until the desired starting character is at the top (or index = 0)
+        while(this.rotorValues.charAt(0) != c) {
+            this.rotate(); 
+        }
             
     }
     
     public boolean rotate(){
-        //TODO
-               
+        // finds last character in rotorValues 
+        char last = rotorValues.charAt(rotorValues.length() - 1); 
+
+        // moves this character to the front of rotorValues 
+        rotorValues = last + rotorValues.substring(0, rotorValues.length() - 1); 
+
+        // if back to starting orientation, return true 
+        return rotorValues.charAt(0) == startChar; 
     }
     
 
